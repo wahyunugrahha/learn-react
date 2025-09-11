a31df6b33a4590e7c61fff102300eae190e49190
wahyunugrahha <wahyunih297@gmail.com>
2025-09-11 21:37:07 +0700

    update

--- 3-catatan/src/App.jsx
+++ 3-catatan/src/App.jsx
@@ -1,53 +1,154 @@
+import { useState } from "react";
+
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+
 export default function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  }
   return (
     <>
       <div className="app">
-        <h1>Catatan Belanjaku 📝</h1>
-        <form className="add-form">
-          <h3>Hari ini belanja apa kita?</h3>
-          <div>
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-            <input type="text" placeholder="nama barang..." />
-          </div>
-          <button>Tambah</button>
-        </form>
-        <div className="list">
-          <ul>
-            <li>
-              <input type="checkbox" checked="true" />
-              <span style="text-decoration: line-through;">1 Kopi</span>
-              <button>&times;</button>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <span>5 Gula Pasir</span>
-              <button>&times;</button>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <span>3 Air Mineral</span>
-              <button>&times;</button>
-            </li>
-          </ul>
-        </div>
-        <div className="actions">
-          <select>
-            <option value="input">Urutkan berdasarkan urutan input</option>
-            <option value="name">Urutkan berdasarkan nama barang</option>
-            <option value="checked">Urutkan berdasarkan ceklis</option>
-          </select>
-          <button>Bersihkan Daftar</button>
-        </div>
-        <footer className="stats">
-          Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
-        </footer>
+        <Header />
+        <Form onAddItem={handleAddItem} />
+        <GroceryList
+          items={items}
+          onDeleteItem={handleDeleteItem}
+          onToggleItem={handleToggleItem}
+        />
+        <Footer />
       </div>
     </>
+  );
+}
+
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+function Form({ onAddItem }) {
+  const [name, setName] = useState("");
+  const [quantity, setQuantitiy] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name) return;
+
+    const newItem = { name, quantity, checkd: false, id: Date.now };
+    onAddItem(newItem);
+    setName("");
+    setQuantitiy(1);
+  }
+
+  const quantityNum = [...Array(20)].map((_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ));
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantitiy(Number(e.target.value))}
+        >
+          {quantityNum}
+        </select>
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function GroceryList({ items, onDeleteItem, onToggleItem }) {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {items.map((item) => (
+            <Item
+              item={item}
+              key={item.id}
+              onDeleteItem={onDeleteItem}
+              onToggleItem={onToggleItem}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
+
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li key={item.id}>
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.checked ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="stats">
+      Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+    </footer>
   );
 }
